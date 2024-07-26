a272028f39e0d00a5a749924de515be579aaca8f
Riley Grotenhuis <riley@Rileys-MacBook-Air.local>
2024-07-26 18:12:49 -0500

    add all experiences

--- resources/js/Pages/Partials/Experience.vue
+++ resources/js/Pages/Partials/Experience.vue
@@ -1,55 +1,84 @@
+<script setup>
+const experiences = [
+  {
+    color: 'primary-600',
+    timeline: 'Nov 2021 - Aug 2024',
+    position: 'Software Engineer',
+    company: 'Midwestern Interactive',
+    technology:
+      'Php, Laravel, JavaScript, TypeScript, Vue, Next.js, React, Docker, Digital Ocean, and more.',
+    details: [
+      'Designed and implemented a comprehensive alarm outage map for a major client, significantly enhancing service reliability across cable, internet, and telephone services in various cities.',
+      'Engineered software solutions for an alternative school lunch program, efficiently handling thousands of real time transactions daily across numerous schools on the East Coast',
+      'Led a development team, conducting regular meetings to support professional growth and skill enhancement, which boosted team performance and improved project outcomes.',
+    ],
+  },
+  {
+    color: 'red-500',
+    timeline: 'Jan 2020 - Nov 2021',
+    position: 'Web Developer',
+    company: 'Missouri State University',
+    technology: 'C#, JavaScript, Python, Asp.NET, React, and more.',
+    details: [
+      'Collaborated with developers, designers, and content strategists to deliver digital solutions for the\n' +
+        'university s extensive community of students, faculty, and staff.',
+      'Utilized scripting and external APIs to successfully migrate tens of thousands of digital assets\n' +
+        'between CMS platforms, significantly reducing manual effort and saving time.',
+      'Partnered with the music department to create an online repository for master s students, enabling\n' +
+        'them to publish and showcase multi medium thesis projects.',
+    ],
+  },
+];
+</script>
+
 <template>
   <div
     class="max-w-6xl mx-auto py-24 px-8 flex flex-col-reverse md:flex-row gap-8 md:gap-0 justify-between items-center"
   >
-    <!-- Midwestern Interactive -->
-    <div class="flex flex-col md:flex-row gap-20">
-      <!-- Overview -->
-      <div class="w-full md:w-1/2">
-        <!-- Timeline -->
-        <div class="text-base font-semibold text-gray-300 mb-8">
-          Nov 2021 - Aug 2024
+    <div class="flex flex-col gap-32">
+      <!-- Experience -->
+      <div
+        v-for="(experience, index) in experiences"
+        :key="index"
+        class="flex flex-col md:flex-row gap-20"
+      >
+        <!-- Overview -->
+        <div class="w-full md:w-1/2">
+          <!-- Timeline -->
+          <div class="text-base font-semibold text-gray-300 mb-8">
+            {{ experience.timeline }}
+          </div>
+
+          <div class="mb-8">
+            <!-- Position -->
+            <h3 class="text-3xl font-bold mb-1">{{ experience.position }},</h3>
+            <!-- Company -->
+            <h5 :class="`text-3xl text-${experience.color} font-bold`">
+              {{ experience.company }}
+            </h5>
+          </div>
+
+          <!-- Technologies -->
+          <div>
+            <p class="text-gray-400 font-semibold">
+              {{ experience.technology }}
+            </p>
+          </div>
         </div>
 
-        <!-- Position & Company -->
-        <div class="mb-8">
-          <h3 class="text-3xl font-bold mb-1">Software Engineer,</h3>
-
-          <h5 class="text-3xl text-primary-600 font-bold">
-            Midwestern Interactive
-          </h5>
+        <!-- Description -->
+        <div class="w-full md:w-1/2">
+          <ul class="flex flex-col gap-8">
+            <li
+              v-for="(detail, index) in experience.details"
+              :key="index"
+              class="text-gray-50 leading-loose"
+            >
+              {{ detail }}
+            </li>
+          </ul>
         </div>
-
-        <!-- Technologies -->
-        <div>
-          <p class="text-gray-400 font-semibold">
-            Php, Laravel, JavaScript, TypeScript, Vue.js, Next.js, React,
-            Docker, Digital Ocean, and more.
-          </p>
-        </div>
-      </div>
-
-      <!-- Description -->
-      <div class="w-full md:w-1/2">
-        <ul class="flex flex-col gap-8">
-          <li class="text-gray-50">
-            Designed and implemented a comprehensive alarm outage map for a
-            major client, significantly enhancing service reliability across
-            cable, internet, and telephone services in various cities
-          </li>
-          <li class="text-gray-50">
-            Engineered software solutions for an alternative school lunch
-            program, efficiently handling thousands of real time transactions
-            daily across numerous schools on the East Coast
-          </li>
-          <li class="text-gray-50">
-            Led a development team, conducting regular meetings to support
-            professional growth and skill enhancement, which boosted team
-            performance and improved project outcomes.
-          </li>
-        </ul>
       </div>
     </div>
   </div>
 </template>
-<script setup lang="ts"></script>
