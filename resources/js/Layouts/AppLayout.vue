1bd8b6205aba994504882bcd32ffeca570699316
Riley Grotenhuis <riley@Rileys-MacBook-Air.local>
2024-07-26 16:54:45 -0500

    wip

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <nav>
+            <h1>Riley Grotenhuis</h1>
+        </nav>
+        <main>
+            <slot />
+        </main>
+    </div>
+</template>
--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,5 +1,11 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+</script>
+
 <template>
-  <div>
-    <h1 class="font-bold text-2xl">Riley Grotenhuis</h1>
-  </div>
+  <AppLayout>
+      <div class="max-w-7xl mx-auto py-24">
+          <h1 class="font-bold text-xl">Riley Grotenhuis.</h1>
+      </div>
+  </AppLayout>
 </template>
