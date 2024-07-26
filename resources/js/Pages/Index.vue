e61ba352e973228de0339edfcb0b9fc8e9cce610
Riley Grotenhuis <riley@Rileys-MacBook-Air.local>
2024-07-26 18:27:17 -0500

    testimonial

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -2,11 +2,13 @@
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Landing from '@/Pages/Partials/Landing.vue';
 import Experience from '@/Pages/Partials/Experience.vue';
+import Testimonial from '@/Pages/Partials/Testimonial.vue';
 </script>
 
 <template>
   <AppLayout>
     <Landing />
     <Experience />
+    <Testimonial />
   </AppLayout>
 </template>
---
+++ resources/js/Pages/Partials/Testimonial.vue
@@ -0,0 +1,26 @@
+<script setup>
+import MwiLogo from '@/Components/Icons/MwiLogo.vue';
+</script>
+
+<template>
+  <div class="w-full py-24 bg-[#27272A]">
+    <div class="max-w-6xl mx-auto px-8">
+      <!-- Sender Info -->
+      <div class="mb-4">
+        <a href="https://www.buildmidwestern.com" target="_blank">
+          <MwiLogo />
+        </a>
+      </div>
+
+      <!-- Message -->
+      <div>
+        <p class="text-2xl font-semibold leading-relaxed mb-2">
+          “He consistently goes the extra mile, not only to nurture his own
+          growth but also to uplift those around him… Riley’s progress has been
+          nothing short of inspiring.”
+        </p>
+        <p class="text-right text-lg leading-relaxed">- Employee Spotlight</p>
+      </div>
+    </div>
+  </div>
+</template>
